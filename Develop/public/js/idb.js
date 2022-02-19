fc3135c63b38db3fb33d4fde8f0c083f07cb3d9b
PacDraft <Kirsten@PacificDraftWorks.com>
2022-02-18 22:17:39 -0800

    added manifest.json file and initial connections for indexedDB

--- Develop/public/js/idb.js
+++ Develop/public/js/idb.js
@@ -1,4 +1,5 @@
 // create variable to hold db connection
 let db;
-// establish a connection to IndexedDB database called 'pizza_hunt' and set it to version 1
+
+// establish a connection to IndexedDB database
 const request = indexedDB.open('budget-tracker', 1);
