ea4acdff44cc627cdab7d0449676b30120afa738
PacDraft <Kirsten@PacificDraftWorks.com>
2022-02-20 11:30:30 -0800

    working on db code

--- Develop/public/js/idb.js
+++ Develop/public/js/idb.js
@@ -4,6 +4,27 @@ let db;
 // establish a connection to IndexedDB database
 const request = indexedDB.open('budget-tracker', 1);
 
+
 request.onupgradeneeded = function(event) {
     const db = event.target.result;
+    db.createObjectStore('pending-transaction', { autoIncrement: true});
+};
+
+request.onsuccess = function(event) {
+    db = event.target.result;
+    if (navigator.onLine) {
+        checkTransactions();
+    }
+};
+
+request.onerror = function(event) {
+    console.log('error' + event.target.errorCode);
+};
+
+function saveTransaction(transaction) {
+    const newTransaction = db.newTransaction(['pending'], 'readwrite');
+
+    const storeTransaction = newTransaction.objectStore('pending transaction');
+
+    storeTransaction.add(transaction);
 }
