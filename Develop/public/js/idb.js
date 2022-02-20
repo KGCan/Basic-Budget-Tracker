c927da6954940bd13523b601d205217e67df3f0d
PacDraft <Kirsten@PacificDraftWorks.com>
2022-02-19 22:13:57 -0800

    working on idb file, saving to go to bed

--- Develop/public/js/idb.js
+++ Develop/public/js/idb.js
@@ -3,3 +3,7 @@ let db;
 
 // establish a connection to IndexedDB database
 const request = indexedDB.open('budget-tracker', 1);
+
+request.onupgradeneeded = function(event) {
+    const db = event.target.result;
+}
