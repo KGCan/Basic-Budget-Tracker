07a74d058d66105cea8792242514d32f05aaa18f
PacDraft <Kirsten@PacificDraftWorks.com>
2022-02-20 12:34:36 -0800

    working on db code

--- Develop/public/js/idb.js
+++ Develop/public/js/idb.js
@@ -21,10 +21,36 @@ request.onerror = function(event) {
     console.log('error' + event.target.errorCode);
 };
 
-function saveTransaction(transaction) {
+function saveTransaction(recordTransaction) {
     const newTransaction = db.newTransaction(['pending'], 'readwrite');
 
     const storeTransaction = newTransaction.objectStore('pending transaction');
 
-    storeTransaction.add(transaction);
+    storeTransaction.add(recordTransaction);
+};
+
+function checkTransactions() {
+    const newTransaction = db.newTransaction(['pending transaction'], 'readwrite');
+    const storeTransaction = newTransaction.objectStore('pending transaction');
+    const getAllTransactions = storeTransaction.getAll();
+
+    getAllTransactions.onsuccess = function() {
+        if (getAllTransactions.result.length > 0) {
+            fetch('/api/transaction/bulk', {
+                method: 'Post',
+                body: JSON.stringify(getAllTransactions.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*', 'Content-Type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .then(() => {
+                const newTransaction = db.transaction(['pending transaction'], 'readwrite');
+
+                const storeTransaction = newTransaction.objectstore('pending transaction');
+
+                storeTransaction.clear();
+            });
+        }
+    };
 }
